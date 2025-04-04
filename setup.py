4e785f95f969517a6c692fe194195b317d918fe8
Madhav Singh <madhav26266@gmail.com>
2025-04-05 00:26:53 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+  name='Generative AI Project',
+  version="0.0.0",
+  author="Madhav Singh",
+  author_email="madhav26266@gmail.com",
+  packages=find_packages(),
+)
